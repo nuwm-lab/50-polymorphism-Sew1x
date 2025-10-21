72cbed62cb762add3a1f11f8950bcdbe1db62b68
Гавриленко Богдан Геннадійович <126027527+Sew1x@users.noreply.github.com>
2025-10-21 14:57:55 +0300

    Add abstract class and implementations for functions

--- Program.cs
+++ Program.cs
@@ -1,18 +1,60 @@
-﻿using System;
 
-namespace LabWork
-{
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
-    }
+using System;
+
+abstract class DroblinBase {
+	public abstract void InputCoefficients();
+	public abstract void PrintCoefficients();
+	public abstract double Calculate(double x);
+}
+
+class DroblinFunction : DroblinBase {
+	double a1, a0, b1, b0;
+	public override void InputCoefficients() {
+		Console.Write("a1 a0 b1 b0: ");
+		var vals = Console.ReadLine().Split();
+		a1 = double.Parse(vals[0]); a0 = double.Parse(vals[1]);
+		b1 = double.Parse(vals[2]); b0 = double.Parse(vals[3]);
+	}
+	public override void PrintCoefficients() {
+		Console.WriteLine($"({a1}*x+{a0})/({b1}*x+{b0})");
+	}
+	public override double Calculate(double x) {
+		var denom = b1*x+b0;
+		if (denom==0) { Console.WriteLine("Знаменник=0"); return double.NaN; }
+		return (a1*x+a0)/denom;
+	}
+}
+
+class DroblinQuadraticFunction : DroblinBase {
+	double a2, a1, a0, b2, b1, b0;
+	public override void InputCoefficients() {
+		Console.Write("a2 a1 a0 b2 b1 b0: ");
+		var vals = Console.ReadLine().Split();
+		a2 = double.Parse(vals[0]); a1 = double.Parse(vals[1]); a0 = double.Parse(vals[2]);
+		b2 = double.Parse(vals[3]); b1 = double.Parse(vals[4]); b0 = double.Parse(vals[5]);
+	}
+	public override void PrintCoefficients() {
+		Console.WriteLine($"({a2}*x^2+{a1}*x+{a0})/({b2}*x^2+{b1}*x+{b0})");
+	}
+	public override double Calculate(double x) {
+		var denom = b2*x*x+b1*x+b0;
+		if (denom==0) { Console.WriteLine("Знаменник=0"); return double.NaN; }
+		return (a2*x*x+a1*x+a0)/denom;
+	}
+}
+
+class Program {
+	static void Main() {
+		while (true) {
+			Console.Write("1-лінійна, 2-квадр, вихід: ");
+			var ch = Console.ReadLine();
+			DroblinBase func = ch=="1" ? new DroblinFunction() : ch=="2" ? new DroblinQuadraticFunction() : null;
+			if (func==null) break;
+			func.InputCoefficients();
+			func.PrintCoefficients();
+			Console.Write("x0: ");
+			if (double.TryParse(Console.ReadLine(), out double x0))
+				Console.WriteLine($"f({x0}) = {func.Calculate(x0)}");
+		}
+	}
 }
